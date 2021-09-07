a29c6e34565f792c23b9fde6b1138d078d76c587
tambonbon <nttammy@yahoo.com>
2021-09-07 12:27:09 +0200

    Done chapter 6: Purely functional state

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -30,19 +30,68 @@ object RNG {
       (f(a), rng2)
     }
 
-  def nonNegativeInt(rng: RNG): (Int, RNG) = ???
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val (number, random) = rng.nextInt
+    (if (number < 0) -(number+1) else number, random) // to avoid the corner case of number being MINValue
+  }
 
-  def double(rng: RNG): (Double, RNG) = ???
+  // generate a DOuble between 0 and 1 (exclusive)
+  def double(rng: RNG): (Double, RNG) = {
+    val (number, random) = nonNegativeInt(rng)
+    (number/(Int.MaxValue.toDouble + 1), random)
+  }
 
-  def intDouble(rng: RNG): ((Int,Double), RNG) = ???
+  def intDouble(rng: RNG): ((Int,Double), RNG) = {
+    val (int, random1) = rng.nextInt
+    val (doub, random2) = double(random1)
+    ((int, doub), random2)
+  }
 
-  def doubleInt(rng: RNG): ((Double,Int), RNG) = ???
+  def doubleInt(rng: RNG): ((Double,Int), RNG) = {
+    val ((int, doub), rand) = intDouble(rng)
+    ((doub, int), rand)
+  }
 
-  def double3(rng: RNG): ((Double,Double,Double), RNG) = ???
+  def double3(rng: RNG): ((Double,Double,Double), RNG) = {
+    val (double1, rand1) = double(rng)
+    val (double2, rand2) = double(rand1)
+    val (double3, rand3) = double(rand2)
+    ((double1, double2, double3), rand3)
+  }
+     // too many duplications...
 
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = ???
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    if (count < 0) (List(), rng)
+    else {
+      val (x, rand1) = rng.nextInt
+      val (xs, rand2) = ints(count-1)(rng)
+      ((x :: xs), rand2)
+    }
+  }
 
-  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
+
+  // 6.5
+  /* 
+  def map[A,B](s: Rand[A])(f: A => B): Rand[B] =
+    rng => {
+      val (a, rng2) = s(rng)
+      (f(a), rng2)
+    } */
+  def doubleByMap: Rand[Double] = { // Rand is RNG => (A, RNG)
+    map(nonNegativeInt)(_ / Int.MaxValue.toDouble + 1)
+    //  ^^^^^^^^^^^^^^  ^^ 
+    //  Rand[Double]   (f: Int => Double)
+  } 
+
+  // map2 takes 2 actions ra and rb, and a function f for combining their results..
+  // .. and returns a new action combining them
+  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
+    rng => {
+      val (val1, rand1) = ra(rng)
+      val (val2, rand2) = rb(rand1)
+      (f(val1, val2), rand2)
+    }
+  }
 
   def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
 
